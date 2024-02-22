4dec2d98b8a32c0d83bd624e64abfe0d6be86b27
akifst <akifst@code.edu.az>
2024-02-22 21:48:22 +0400

    method

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1ci ededi daxil et");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("2ci eded daxil et");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("operator daxil et");
+            string simvol = (Console.ReadLine());
+            Console.WriteLine(eded(num1, num2, simvol));
+             
+
+        }
+        static int eded(int num1, int num2, string simvol)
+        {
+            int result = 0;
+            if (simvol == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (simvol == "-")
+            {
+                result= num1 - num2;
+            }
+            else if (simvol == "/")
+            {
+                if ( num2 == 0)
+                {
+                    Console.WriteLine("0-a bolme yoxdur");
+                }
+                else
+                {
+                    result = num1 / num2;
+                }
+                
+            }
+
+            else if (simvol == "*")
+            {
+                result= num1 * num2;
+            }
+            else
+            {
+                Console.WriteLine("duzgun eded daxil et");
+            }
+            return result;
+        }
+
+
+    }
+}
