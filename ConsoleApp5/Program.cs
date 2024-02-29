c4653b43c6770777e1a97ea3d2f92851e740e548
akifst <akifst@code.edu.az>
2024-02-29 20:17:06 +0400

    feat:method

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.Tracing;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine(StringName("salam"));
+            //string[] words = { "salam", "limon", "necesen" };
+
+            //ReversedItemOfArray(words);
+        }
+
+        //static string StringName(string name)
+        //{
+        //    string newWord = String.Empty;
+        //    for (int i = 0; i < name.Length; i++)
+        //    {
+        //        if (i== name.Length-1)
+        //        {
+        //            newWord += name[i];
+        //        }
+        //        else
+        //        {
+        //            newWord += name[i] + "*";
+        //        }
+
+        //    }
+        //    return newWord;
+        //}
+
+
+        //static void ReversedItemOfArray(string[] array)
+        //{
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        String reversedString = ReverseString(array[i]);
+        //        array[i] = reversedString;  
+        //    }
+        //    ShowArray(array);
+        //}
+        //static void ShowArray(string[] array)
+        //{
+        //    foreach (var item in array)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //}
+        //static string ReverseString(string word)
+        //{
+        //    string reversedWord = String.Empty;
+        //    for (int i = word.Length - 1; i >= 0; i--)
+        //    {
+        //        reversedWord += word[i];
+        //    }
+        //    return reversedWord;
+        //}
+
+
+    }
+}
